e9068892ca0dd901c813f98fb04142b5db1dd052
Devdatt17 <devdattpandit99@gmail.com>
2021-05-09 18:32:41 +0530

    Quiz questions feature added

--- js/script.js
+++ js/script.js
@@ -61,7 +61,7 @@ startGame = () => {
 getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
-        return window.location.assign('/end.html');
+        return window.location.assign('./end.html'); //This is not working because of security concerns
     }
     questionCounter++;
     progressText.innerHTML = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
